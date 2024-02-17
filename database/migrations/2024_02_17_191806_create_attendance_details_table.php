6a9cd33d4b1bdb4c8e8570aa10d4974bab81f910
Faisal-dev5 <sendipede093@gmail.com>
2024-02-18 02:24:30 +0700

    Installation Laravel and design table Laravel project

---
+++ database/migrations/2024_02_17_191806_create_attendance_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('attendance_id');
+            $table->string('long');
+            $table->string('lat');
+            $table->string('address');
+            $table->string('photo');
+            $table->enum('type',['in','out']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance_details');
+    }
+};
